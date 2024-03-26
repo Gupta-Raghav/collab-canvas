c94b29eec124a000415f277cb2ed9f83b1fe099f
Gupta-Raghav <42888633+Gupta-Raghav@users.noreply.github.com>
2024-03-26 17:51:27 -0400

    added an erase button

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,16 @@ const page: FC<PageProps> = ({}) => {
 
   const {canvasRef, onMouseDown, onClear} = useDraw(drawline)
   const [color, setColor] = useState<string>('#000')
+  const [width, setwidth] = useState<number>(5)
+  const eraseHandler = () => {
+    setColor('#fff')
+    setwidth(20)
+  }
+
   function drawline({prevPoint, currentPoint, ctx}:Draw) {
     
     const lineColor = color
-    const lineWidth = 5
+    const lineWidth = width
 
     let startPoint = prevPoint ?? currentPoint
     ctx.beginPath()
@@ -37,9 +43,14 @@ const page: FC<PageProps> = ({}) => {
     <div className='h-screen flex justify-center items-center'>
      <div className="flex flex-col gap-10 pr-10">
      <ColorPicker color={color} className="self-start" onChange={(e) => setColor(e.hex)} />
-      <button type='button' className='flex justify-between items-center w-fit gap-2 p-2 rounded-md border border-black' onClick={onClear}>
-        Clear <FaEraser />
+     <div className='flex justify-between'>
+     <button type='button' className='flex justify-between items-center w-fit gap-2 p-2 rounded-md border border-black' onClick={() => eraseHandler()}>
+        Erase <FaEraser />
       </button>
+      <button type='button' className='flex justify-between items-center w-fit gap-2 p-2 rounded-md border border-black' onClick={onClear}>
+        Clear 
+      </button>
+     </div>
      </div>
       <canvas onMouseDown={onMouseDown} ref={canvasRef} height={750} width={500} className='border border-black rounded-md'/>
     </div>
